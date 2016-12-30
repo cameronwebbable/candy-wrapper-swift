fdd4edfa10886ee7f83bd146ece732c31ca99b19
cameronwebbable <cwebb02@gmail.com>
2016-12-30 11:10:55 -0600

    minor adds

---
+++ CandyWrapperSwift/CandyWrapper.swift
@@ -0,0 +1,17 @@
+//
+//  CandyWrapper.swift
+//  CandyWrapperSwift
+//
+//  Created by Cameron Webb on 12/30/16.
+//  Copyright © 2016 Cameron Webb. All rights reserved.
+//
+
+import Foundation
+
+public class CandyWrapper {
+    public var baseURL: String?
+    
+    init(baseURL: String) {
+        self.baseURL = baseURL
+    }
+}
