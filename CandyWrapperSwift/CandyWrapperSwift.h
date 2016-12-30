8c7707c0604fd269814c051aac8112d97d82d3b1
cameronwebbable <cwebb02@gmail.com>
2016-12-30 10:39:02 -0600

    Initial Commit

---
+++ CandyWrapperSwift/CandyWrapperSwift.h
@@ -0,0 +1,19 @@
+//
+//  CandyWrapperSwift.h
+//  CandyWrapperSwift
+//
+//  Created by Cameron Webb on 12/30/16.
+//  Copyright © 2016 Cameron Webb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CandyWrapperSwift.
+FOUNDATION_EXPORT double CandyWrapperSwiftVersionNumber;
+
+//! Project version string for CandyWrapperSwift.
+FOUNDATION_EXPORT const unsigned char CandyWrapperSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CandyWrapperSwift/PublicHeader.h>
+
+
